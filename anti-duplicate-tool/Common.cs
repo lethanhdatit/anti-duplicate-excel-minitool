86205ec6c89de79473a3924d9315e04f38997eee
Dat Le Thanh (SM) <dat.le.thanh@dmchosting.ca>
2023-08-21 18:08:33 +0700

    applying new logic

---
+++ anti-duplicate-tool/Common.cs
@@ -0,0 +1,175 @@
+﻿using IronXL;
+using System;
+
+public enum ItemStatus 
+{
+    NO_CHANGED,
+    ADDED_NEW,
+    CHANGED,
+    REMOVED
+}
+public class Item
+{
+    public string Key { get; set; }
+    public List<string> DuplicatedKeys { get; set; }
+    public object Value { get; set; }
+    public string CompareNote { get; set; }
+    public ItemStatus CompareStatus { get; set; }
+}
+
+public class StringIEqualityComparer : IEqualityComparer<string>
+{
+    public bool Equals(string x, string y)
+    {
+        return x.Equals(y, StringComparison.OrdinalIgnoreCase);
+    }
+
+    public int GetHashCode(string obj)
+    {
+        return obj.GetHashCode();
+    }
+}
+
+public static class Common
+{
+    public static string BuildNewKey(string prefix, List<string> duplicatedKeys, int levelPrefix)
+    {
+        string newKey = duplicatedKeys.FirstOrDefault(f => !f.Contains(".")) ?? duplicatedKeys.Select(s => new { count = s.Count(c => c == '.'), key = s }).OrderByDescending(s => s.count).FirstOrDefault().key;
+
+        if (newKey.Contains("."))
+        {
+            var splited = newKey.Split('.');
+            var suffix = string.Empty;
+            for (int i = 1; i <= levelPrefix; i++)
+            {
+                if (splited.Length - i >= 0)
+                {
+                    suffix = splited[splited.Length - i] + (!string.IsNullOrWhiteSpace(suffix) ? "." : string.Empty) + suffix;
+                }
+            }
+            newKey = $"{prefix}.{suffix}";
+        }
+
+        return newKey;
+    }
+
+    public static void ProcessMetadata(int levelPrefix, string common_fe_prefix, string common_be_prefix, string beKeyword, WorkSheet wsTemp, IEnumerable<IGrouping<object, Cell>> uniqueListValues, List<Item> duplicatedFeKeyGroups, List<Item> duplicatedBeKeyGroups, out List<IGrouping<string, Item>> dupkeyFe, out List<IGrouping<string, Item>> dupkeyBe)
+    {
+        foreach (var item in uniqueListValues)
+        {
+            List<string> duplicatedFeKeys = new List<string>();
+            List<string> duplicatedBeKeys = new List<string>();
+            foreach (var ii in item)
+            {
+                var key = wsTemp[$"B{ii.RowIndex + 1}"].StringValue;
+
+                if (!string.IsNullOrWhiteSpace(key))
+                {
+                    if (key.Contains(beKeyword, StringComparison.OrdinalIgnoreCase))
+                        duplicatedBeKeys.Add(key);
+                    else
+                        duplicatedFeKeys.Add(key);
+                }
+            }
+
+            string newFeKey = string.Empty;
+            if (duplicatedFeKeys.Any())
+            {
+                newFeKey = duplicatedFeKeys.Count > 1 ? Common.BuildNewKey(common_fe_prefix, duplicatedFeKeys, levelPrefix) : duplicatedFeKeys.FirstOrDefault();
+                duplicatedFeKeyGroups.Add(new Item { Key = newFeKey, DuplicatedKeys = duplicatedFeKeys, Value = item.Key });
+            }
+
+            string newBeKey = string.Empty;
+            if (duplicatedBeKeys.Any())
+            {
+                newBeKey = duplicatedBeKeys.Count > 1 ? Common.BuildNewKey(common_be_prefix, duplicatedBeKeys, levelPrefix) : duplicatedBeKeys.FirstOrDefault();
+                duplicatedBeKeyGroups.Add(new Item { Key = newBeKey, DuplicatedKeys = duplicatedBeKeys, Value = item.Key });
+            }
+        }
+
+        dupkeyFe = duplicatedFeKeyGroups.GroupBy(g => g.Key).Where(a => a.Count() > 1).ToList();
+        foreach (var group in dupkeyFe)
+            duplicatedFeKeyGroups.RemoveAll(s => s.Key == group.Key);
+
+        dupkeyBe = duplicatedBeKeyGroups.GroupBy(g => g.Key).Where(a => a.Count() > 1).ToList();
+        foreach (var group in dupkeyBe)
+            duplicatedBeKeyGroups.RemoveAll(s => s.Key == group.Key);
+    }
+
+
+    public static void ProcessCompare(WorkSheet? rWs, List<Item> currentKeyGroups, List<Item> compareResults, List<Item> compareWithKeyGroups)
+    {
+        List<Item> readOnlyItems = new List<Item>();
+
+        foreach (var currentRow in currentKeyGroups)
+        {
+            var exitedValue = compareWithKeyGroups.FirstOrDefault(a => a.Value.ToString() == currentRow.Value.ToString());
+            var exitedKey = compareWithKeyGroups.FirstOrDefault(a => a.Key == currentRow.Key);
+
+            if (exitedValue == null)
+            {
+                currentRow.CompareNote += $"- New VALUE detected (column '{rWs["E1"].Value}')" + Environment.NewLine;
+                currentRow.CompareStatus = ItemStatus.ADDED_NEW;
+            }
+
+            if (exitedKey == null)
+            {
+                currentRow.CompareNote += $"- New KEY detected (column '{rWs["C1"].Value}')" + Environment.NewLine;
+                currentRow.CompareStatus = ItemStatus.ADDED_NEW;
+            }
+
+            if (exitedKey != null)
+            {
+                foreach (var cr in currentRow.DuplicatedKeys)
+                {
+                    var newDup = exitedKey.DuplicatedKeys.FirstOrDefault(f => f == cr);
+                    if (newDup == null)
+                    {
+                        currentRow.CompareNote += $"'{cr}' added" + Environment.NewLine;
+                        currentRow.CompareStatus = ItemStatus.CHANGED;
+                    }
+                }
+                foreach (var old in exitedKey.DuplicatedKeys)
+                {
+                    var oldDup = currentRow.DuplicatedKeys.FirstOrDefault(f => f == old);
+                    if (oldDup == null)
+                    {
+                        currentRow.CompareNote += $"'{old}' removed" + Environment.NewLine;
+                        currentRow.CompareStatus = ItemStatus.CHANGED;
+                    }
+                }
+            }
+        }
+
+        foreach (var compareWithRow in compareWithKeyGroups)
+        {
+            var exitedValue = currentKeyGroups.FirstOrDefault(a => a.Value.ToString() == compareWithRow.Value.ToString());
+            var exitedKey = currentKeyGroups.FirstOrDefault(a => a.Key == compareWithRow.Key);
+
+            Item temp = new Item
+            {
+                Key = compareWithRow.Key,
+                DuplicatedKeys = compareWithRow.DuplicatedKeys,
+                Value = compareWithRow.Value
+            };
+
+            if (exitedValue == null)
+            {
+                temp.CompareNote += $"- VALUE not found in current version (column '{rWs["E1"].Value}')" + Environment.NewLine;
+                temp.CompareStatus = ItemStatus.REMOVED;
+            }
+
+            if (exitedKey == null)
+            {
+                temp.CompareNote += $"- KEY not found in current version (column '{rWs["C1"].Value}')" + Environment.NewLine;
+                temp.CompareStatus = ItemStatus.REMOVED;
+            }
+
+            if (!string.IsNullOrWhiteSpace(temp.CompareNote))
+                currentKeyGroups.Add(temp);
+        }
+
+        compareResults.AddRange(currentKeyGroups.Where(w => w.CompareStatus != ItemStatus.NO_CHANGED));
+        currentKeyGroups.RemoveAll(w => w.CompareStatus != ItemStatus.NO_CHANGED);
+    }
+}
--- anti-duplicate-tool/Program.cs
+++ anti-duplicate-tool/Program.cs
@@ -7,24 +7,33 @@ bool excelProcess = true;
 bool jsonProcess = true;
 bool autoSizeRow = true;
 bool autoSizeColumn = true;
-
-string version = "v3";
 int levelPrefix = 1;
-string path = "C:\\Users\\Admin\\Desktop\\duplicate\\";
 
-string i_excelPath = $"{path}en_flat.xlsx";
-string o_excelPath = $"{path}en_flat_unique_{version}.xlsx";
+string version = "v4";
+string compareWithVersion = "v3";
+
+string path = "C:\\Users\\Admin\\Desktop\\duplicate\\";
+string prefixFileName = "en_flat";
+
+string i_excelPath = $"{path}{prefixFileName}_{version}.xlsx";
+string i_jsonPath = $"{path}{prefixFileName}_{version}.json";
+
+string o_excelPath = $"{path}{prefixFileName}_unique_{version}.xlsx";
+string o_jsonPath = $"{path}{prefixFileName}_unique_{version}.json";
+
+string c_excelPath = $"{path}{prefixFileName}_{compareWithVersion}.xlsx";
+
+string dup_jsonPath = $"{path}{prefixFileName}_dup_{version}.json";
 string t_excelPath = $"{path}__temp__.xlsx";
-string i_jsonPath = $"{path}en_flat.json";
-string o_jsonPath = $"{path}en_flat_unique_{version}.json";
-string diff_jsonPath = $"{path}en_flat_diff_{version}.json";
-string dup_jsonPath = $"{path}en_flat_dup_{version}.json";
+
 string common_fe_prefix = "common";
 string common_be_prefix = "common.backend";
 string beKeyword = "backendService";
 
 if (!excelProcess && !jsonProcess)
     return;
+
+bool processCompare = !string.IsNullOrWhiteSpace(compareWithVersion);
 
 var wb_original = WorkBook.Load(i_excelPath);
 if(wb_original != null)
@@ -40,11 +49,12 @@ if(wb_original != null)
     var wsTemp = wbTemp.GetWorkSheet("en") ?? wbTemp.DefaultWorkSheet;
 
     int colValueIndex = 2; // en   
+    string checkDupTargetColumn = wsTemp.Columns[colValueIndex].Rows[0].Value.ToString();
 
     // keep current char case
     var uniqueListValues = wsTemp.Columns[colValueIndex]
                              .Where(s => s.RowIndex != 0) // not count header row
-                             .GroupBy(g => g.Value);
+                             .GroupBy(g => g.Value.ToString());
 
     WorkBook? rWb = excelProcess ? WorkBook.Create(ExcelFileFormat.XLSX) : null;
     WorkSheet? rWs = null;
@@ -53,68 +63,85 @@ if(wb_original != null)
         rWb.DefaultWorkSheet.Name = wsTemp.Name;
         rWs = rWb.DefaultWorkSheet;
 
-        rWs["A1"].Value = "assign_to";
+        rWs["A1"].Value = "compare_status" + (processCompare ? $"_with_{compareWithVersion}" : string.Empty);
         rWs["A1"].Style.Font.Bold = true;
 
-        rWs["B1"].Value = "new_key_name";
+        rWs["B1"].Value = "compare_note" + (processCompare ? $"_with_{compareWithVersion}" : string.Empty);
         rWs["B1"].Style.Font.Bold = true;
 
-        rWs["C1"].Value = "duplicated_keys";
+        rWs["C1"].Value = "new/existed_key";
         rWs["C1"].Style.Font.Bold = true;
 
-        rWs["D1"].Value = "en_unique";
+        rWs["D1"].Value = "duplicated_keys";
         rWs["D1"].Style.Font.Bold = true;
+
+        rWs["E1"].Value = $"{checkDupTargetColumn}_unique";
+        rWs["E1"].Style.Font.Bold = true;
     }
-   
+
     int i = 2;
     List<Item> duplicatedFeKeyGroups = new List<Item>();
     List<Item> duplicatedBeKeyGroups = new List<Item>();
-    foreach (var item in uniqueListValues)
-    {
-        List<string> duplicatedFeKeys = new List<string>();
-        List<string> duplicatedBeKeys = new List<string>();
-        foreach (var ii in item)
-        {
-            var key = wsTemp[$"B{ii.RowIndex + 1}"].StringValue;
-           
-            if (!string.IsNullOrWhiteSpace(key))
-            {
-                if(key.Contains(beKeyword, StringComparison.OrdinalIgnoreCase))
-                    duplicatedBeKeys.Add(key);
-                else
-                    duplicatedFeKeys.Add(key);
-            }
-        }
+    List<Item> compareResults = new List<Item>();
+    List<IGrouping<string, Item>> dupkeyFe, dupkeyBe;
 
-        string newFeKey = string.Empty;
-        if (duplicatedFeKeys.Any())
-        {
-            newFeKey = duplicatedFeKeys.Count > 1 ? BuildNewKey(common_fe_prefix, duplicatedFeKeys, levelPrefix) : duplicatedFeKeys.FirstOrDefault();
-            duplicatedFeKeyGroups.Add(new Item { NewKey = newFeKey, DuplicatedKeys = duplicatedFeKeys, Value = item.Key });
-        }
-
-        string newBeKey = string.Empty;
-        if (duplicatedBeKeys.Any())
-        {
-            newBeKey = duplicatedBeKeys.Count > 1 ? BuildNewKey(common_be_prefix, duplicatedBeKeys, levelPrefix) : duplicatedBeKeys.FirstOrDefault();
-            duplicatedBeKeyGroups.Add(new Item { NewKey = newBeKey, DuplicatedKeys = duplicatedBeKeys, Value = item.Key });
-        }
-    }
-
-    var dupkeyFe = duplicatedFeKeyGroups.GroupBy(g => g.NewKey).Where(a => a.Count() > 1).ToList();
-
-    foreach (var group in dupkeyFe)
-        duplicatedFeKeyGroups.RemoveAll(s => s.NewKey == group.Key);
+    Common.ProcessMetadata(levelPrefix, common_fe_prefix, common_be_prefix, beKeyword, wsTemp, uniqueListValues, duplicatedFeKeyGroups, duplicatedBeKeyGroups, out dupkeyFe, out dupkeyBe);
 
     if (excelProcess)
     {
-        foreach (var fe in duplicatedFeKeyGroups)
+        if (processCompare)
         {
-            rWs[$"B{i}"].Value = fe.NewKey;
-            rWs[$"C{i}"].Value = string.Join(Environment.NewLine, fe.DuplicatedKeys);
-            rWs[$"D{i}"].Value = fe.Value;
+            if (!File.Exists(c_excelPath))
+                Console.WriteLine($"Target to compare not found. path: {c_excelPath}");
+            else
+            {
+                var cWb = WorkBook.Load(c_excelPath);
+                var cWs = cWb.GetWorkSheet("en") ?? cWb.DefaultWorkSheet;
 
-            rWs[$"C{i}"].Style.WrapText = true;
+                List<Item> compareFeKeyGroups = new List<Item>();
+                List<Item> compareBeKeyGroups = new List<Item>();
+                List<IGrouping<string, Item>> compareDupkeyFe, compareDupkeyBe;
+
+                var compareUniqueListValues = cWs.Columns[colValueIndex]
+                                                 .Where(s => s.RowIndex != 0) // not count header row
+                                                 .GroupBy(g => g.Value.ToString());
+
+                Common.ProcessMetadata(levelPrefix, common_fe_prefix, common_be_prefix, beKeyword, cWs, compareUniqueListValues, compareFeKeyGroups, compareBeKeyGroups, out compareDupkeyFe, out compareDupkeyBe);
+
+                Common.ProcessCompare(rWs, duplicatedFeKeyGroups, compareResults, compareFeKeyGroups);
+
+                Common.ProcessCompare(rWs, duplicatedBeKeyGroups, compareResults, compareBeKeyGroups);
+            }
+        }
+
+        rWs.AutoSizeRow(0);
+
+        foreach (var cp in compareResults)
+        {
+            rWs[$"B{i}"].Value = cp.CompareNote;
+            rWs[$"A{i}"].Value = cp.CompareStatus.ToString();
+            rWs[$"C{i}"].Value = cp.Key;
+            rWs[$"D{i}"].Value = string.Join(Environment.NewLine, cp.DuplicatedKeys);
+            rWs[$"E{i}"].Value = cp.Value;
+
+            rWs[$"B{i}"].Style.WrapText = true;
+            rWs[$"D{i}"].Style.WrapText = true;
+
+            if(cp.CompareStatus == ItemStatus.ADDED_NEW)
+            {
+                rWs[$"A{i}"].Style.Font.Color = "#05F50D";
+                rWs[$"C{i}:E{i}"].Style.Font.Color = "#05F50D";
+            }
+            else if(cp.CompareStatus == ItemStatus.REMOVED)
+            {
+                rWs[$"A{i}"].Style.Font.Color = "#F50505";
+                rWs[$"C{i}:E{i}"].Style.Font.Color = "#F50505";
+            }
+            else if (cp.CompareStatus == ItemStatus.CHANGED)
+            {
+                rWs[$"A{i}"].Style.Font.Color = "#F57D05";
+                rWs[$"D{i}"].Style.Font.Color = "#F57D05";
+            }
 
             if (autoSizeRow)
                 rWs.AutoSizeRow(i - 1);
@@ -122,14 +149,45 @@ if(wb_original != null)
             i++;
         }
 
-        foreach (var group in dupkeyFe)
-            foreach (var item in group)
+        foreach (var fe in duplicatedFeKeyGroups)
+        {
+            rWs[$"C{i}"].Value = fe.Key;
+            rWs[$"D{i}"].Value = string.Join(Environment.NewLine, fe.DuplicatedKeys);
+            rWs[$"E{i}"].Value = fe.Value;
+
+            rWs[$"D{i}"].Style.WrapText = true;
+
+            if (autoSizeRow)
+                rWs.AutoSizeRow(i - 1);
+
+            i++;
+        }
+
+        foreach (var gr in dupkeyFe)
+            foreach (var item in gr)
                 foreach (var d in item.DuplicatedKeys)
                 {
-                    rWs[$"B{i}"].Value = d;
-                    rWs[$"D{i}"].Value = item.Value.ToString();
+                    rWs[$"C{i}"].Value = d;
+                    rWs[$"D{i}"].Value = "skipped process because duplicated new key -> accept duplicate value";
+                    rWs[$"E{i}"].Value = item.Value.ToString();
 
-                    rWs[$"C{i}"].Style.WrapText = true;
+                    rWs[$"D{i}"].Style.WrapText = true;
+
+                    if (autoSizeRow)
+                        rWs.AutoSizeRow(i - 1);
+
+                    i++;
+                }
+
+        foreach (var gr in dupkeyBe)
+            foreach (var item in gr)
+                foreach (var d in item.DuplicatedKeys)
+                {
+                    rWs[$"C{i}"].Value = d;
+                    rWs[$"D{i}"].Value = "skipped process because duplicated new key -> accept duplicate value";
+                    rWs[$"E{i}"].Value = item.Value.ToString();
+
+                    rWs[$"D{i}"].Style.WrapText = true;
 
                     if (autoSizeRow)
                         rWs.AutoSizeRow(i - 1);
@@ -139,11 +197,11 @@ if(wb_original != null)
 
         foreach (var be in duplicatedBeKeyGroups)
         {
-            rWs[$"B{i}"].Value = be.NewKey;
-            rWs[$"C{i}"].Value = string.Join(Environment.NewLine, be.DuplicatedKeys);
-            rWs[$"D{i}"].Value = be.Value;
+            rWs[$"C{i}"].Value = be.Key;
+            rWs[$"D{i}"].Value = string.Join(Environment.NewLine, be.DuplicatedKeys);
+            rWs[$"E{i}"].Value = be.Value;
 
-            rWs[$"C{i}"].Style.WrapText = true;
+            rWs[$"D{i}"].Style.WrapText = true;
 
             if (autoSizeRow)
                 rWs.AutoSizeRow(i - 1);
@@ -170,28 +228,38 @@ if(wb_original != null)
 
     if (jsonProcess)
     {
-        if (duplicatedFeKeyGroups.Any() || duplicatedBeKeyGroups.Any())
+        if (duplicatedFeKeyGroups.Any() || duplicatedBeKeyGroups.Any() || compareResults.Any())
         {
-            var jObj = JsonConvert.DeserializeObject<JObject>(File.ReadAllText(i_jsonPath));
-            long count = 0;
             JObject combines = new JObject();
 
+            foreach (var gr in compareResults.Where(w => w.CompareStatus != ItemStatus.REMOVED))
+                combines.Add(gr.Key, gr.Value.ToString());
+
             foreach (var gr in duplicatedFeKeyGroups)
-                combines.Add(gr.NewKey, gr.Value.ToString());
+                combines.Add(gr.Key, gr.Value.ToString());
 
             foreach (var group in dupkeyFe)
                 foreach (var item in group)
                     foreach (var d in item.DuplicatedKeys)
                     {
-                        if (jObj.ContainsKey(d) && !combines.ContainsKey(d))
+                        if (!combines.ContainsKey(d))
                         {
                             combines.Add(d, item.Value.ToString());
                         }
                     }
-                        
+
+            foreach (var group in dupkeyBe)
+                foreach (var item in group)
+                    foreach (var d in item.DuplicatedKeys)
+                    {
+                        if (!combines.ContainsKey(d))
+                        {
+                            combines.Add(d, item.Value.ToString());
+                        }
+                    }
 
             foreach (var gr in duplicatedBeKeyGroups)
-                combines.Add(gr.NewKey, gr.Value.ToString());
+                combines.Add(gr.Key, gr.Value.ToString());
 
             File.WriteAllText(o_jsonPath, JsonConvert.SerializeObject(combines, Formatting.Indented));
             var beDups = duplicatedBeKeyGroups.SelectMany(k => k.DuplicatedKeys);
@@ -200,25 +268,4 @@ if(wb_original != null)
             File.WriteAllText(dup_jsonPath, JsonConvert.SerializeObject(new { total = dups.Count(), backend = beDups.Count(), frontend = feDups.Count(), result = dups }, Formatting.Indented));
         }
     }
-}
-
-static string BuildNewKey(string prefix, List<string> duplicatedKeys, int levelPrefix)
-{
-    string newKey = duplicatedKeys.FirstOrDefault(f => !f.Contains(".")) ?? duplicatedKeys.Select(s => new { count = s.Count(c => c == '.'), key = s }).OrderByDescending(s => s.count).FirstOrDefault().key;
-
-    if (newKey.Contains("."))
-    {
-        var splited = newKey.Split('.');
-        var suffix = string.Empty;
-        for (int i = 1; i <= levelPrefix; i++)
-        {
-            if(splited.Length - i >= 0)
-            {
-                suffix = splited[splited.Length - i] + (!string.IsNullOrWhiteSpace(suffix) ? "." : string.Empty) + suffix;
-            }                
-        }
-        newKey = $"{prefix}.{suffix}";
-    }
-
-    return newKey;
 }
